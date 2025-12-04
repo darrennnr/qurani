dcdf4fd8d0179f9268c25d4e9343984bbeb0e434
Gitushrh <riflogame52@gmail.com>
2025-12-04 07:45:55 +0700

    update kode project jadi com.myqurani.hafidz

--- cuda_qurani/android/app/build.gradle.kts
+++ cuda_qurani/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cuda_qurani"
+    namespace = "com.myqurani.hafidz"
     compileSdk = 36
     ndkVersion = "27.3.13750724"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cuda_qurani"
+        applicationId = "com.myqurani.hafidz"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
--- cuda_qurani/android/app/src/main/kotlin/com/example/vosk_stt/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.vosk_stt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
