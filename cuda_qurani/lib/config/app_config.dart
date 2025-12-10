cb23d5d08995bd8f6fd34055e2ac2ae35b7c5716
darrennnr <dpnarendra67@gmail.com>
2025-12-10 14:13:34 +0700

    api bocor asu

--- cuda_qurani/lib/config/app_config.dart
+++ cuda_qurani/lib/config/app_config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
-  static const String websocketUrl = 'ws://192.168.1.40:8000/ws/recite';
+  static String get websocketUrl => dotenv.env['API_URL'] ?? '';
   static String get supabaseUrl => dotenv.env['SUPABASE_URL'] ?? '';
   static String get supabaseAnonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? '';
 }
